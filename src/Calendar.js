cfe188771afe49b353e061f9dab73d3fc8eb3ec7
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2017-05-02 18:58:19 +0100

    Pass navigate action to onNavigate().

--- src/Calendar.js
+++ src/Calendar.js
@@ -563,7 +563,7 @@ class Calendar extends React.Component {
 
    date = moveDate(action, newDate || date, ViewComponent)
 
-   onNavigate(date, view)
+   onNavigate(date, view, action)
  };
 
  handleViewChange = (view) => {
